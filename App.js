3494f41dbd4a7149838804e73665147e2e6bc0f9
Teemu Laine <teemu.laine@myy.haaga-helia.fi>
2022-08-25 12:21:11 +0300

    Finished the exercise logic. Some CSS still to be done

--- App.js
+++ App.js
@@ -1,24 +1,77 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
-  const [guessedNumber, setGuessedNumber] = useState();
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const [typedNumber, setTypedNumber] = useState("");
 
-  const handleChange = () => {};
+  const [guessedNumber, setGuessedNumber] = useState("");
+  const [numberOfGuesses, setNumberOfGuesses] = useState(0);
 
-  return (
-    <View style={styles.container}>
-      <Text>Guess a number between 1-100</Text>
-      <TextInput
-        onChange={(e) => {
-          setGuessedNumber(e.target.value);
-        }}
-        value={guessedNumber}
-      />
-    </View>
-  );
+  const [randomNumber, setRandomNumber] = useState(null);
+
+  const [isWrong, setIsWrong] = useState(false);
+
+  console.log(randomNumber);
+
+  useEffect(() => {
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+  }, []);
+
+  const handleChange = (e) => {
+    setTypedNumber(e.target.value);
+  };
+
+  const handleWrongNumber = () => {
+    setIsWrong(true);
+    setTimeout(() => {
+      setIsWrong(false);
+    }, 3000);
+  };
+
+  const handleSubmit = () => {
+    setNumberOfGuesses(numberOfGuesses + 1);
+    setGuessedNumber(typedNumber);
+    setTypedNumber("");
+    if (parseInt(typedNumber) != randomNumber) {
+      handleWrongNumber();
+    }
+  };
+
+  console.log("Guesses", numberOfGuesses);
+
+  if (parseInt(guessedNumber) == randomNumber) {
+    return (
+      <View styles={styles.container}>
+        <Text>
+          You guessed the right number! It took {numberOfGuesses} guesses. The
+          number was {randomNumber}
+        </Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Guess a number between 1-100</Text>
+        <TextInput
+          style={styles.input}
+          type="text"
+          onChange={handleChange}
+          value={typedNumber}
+        />
+        <Button
+          onPress={handleSubmit}
+          type="button"
+          title="Make a Guess"
+        ></Button>
+        {guessedNumber < randomNumber && guessedNumber ? (
+          <Text borderColor="red">The guess was too low.</Text>
+        ) : null}
+        {guessedNumber > randomNumber && guessedNumber ? (
+          <Text borderColor="red">The guess was too low.</Text>
+        ) : null}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -27,8 +80,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    textAlign: "center",
   },
   input: {
+    margin: 10,
+    height: 30,
+    width: 180,
     borderColor: "black",
     borderWidth: 1,
   },
